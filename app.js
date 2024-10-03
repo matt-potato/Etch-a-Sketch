5df7adee0a78d5aeb837372372fede5f4478dffe
matt-potato <matt.chriest@gmail.com>
2024-10-02 20:11:57 -0500

    refactored + styled

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 'use strict';
 
-const grid = document.querySelector('.grid');
+const grid = document.querySelector('.etch-sketch__grid');
 function createGrid(gridNum) {
     grid.replaceChildren();
     for(let i = 1; i <= gridNum * gridNum; i++) {
         const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
+        gridItem.classList.add('etch-sketch__grid-item');
         gridItem.setAttribute('style', `flex: 0 0 calc(100% / ${gridNum})`);
         grid.append(gridItem);
     }
 }
 
-const button = document.querySelector('.btn_grid');
+const button = document.querySelector('.etch-sketch__btn');
 button.addEventListener('click', function() {
-    let squares = prompt('Number of Squares Per Side');
+    const error = document.querySelector('.etch-sketch__error');
+    let squares = Number(document.querySelector('.etch-sketch__grid-number').value);
 
-    squares <= 100 ? createGrid(squares) : alert('Number of Squares Per Side (Less than 100)');
+    if(squares <= 100 && squares > 0) {
+        error.textContent = '';
+        createGrid(squares);
+    }
+    else {
+        error.textContent = 'Enter less than 100';
+    }
 
-    const gridItem = document.querySelectorAll('.grid-item');
+    const gridItem = document.querySelectorAll('.etch-sketch__grid-item');
     gridItem.forEach((gridItem) => {
         gridItem.addEventListener('mouseenter', function() {
             gridItem.classList.add('colored');
